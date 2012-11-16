5500e0504bcce54db9521f01ee79365572aacfb2
saharacremona <saharacremona@gmail.com>
2012-11-16 09:45:26 +0000

    added HistogramRatio plots and code

--- Code/DataVisualization.m
+++ Code/DataVisualization.m
@@ -27,8 +27,26 @@ for i = 1:n
     figure;
     hold on;
     bar(edges,[G,B*factor],1);
-    title(Labels_Train{i});
+    title(strcat({'Scaled histogram of '},Labels_Train{i}));
+    xlabel(Labels_Train{i});
+    ylabel('Relative Frequency');
     legend('Good','Bad');
-    saveas(gcf, strcat('..\Plots\Histogram_',Labels_Train{i}), 'png')
+    saveas(gcf, strcat('..\Plots\HistogramScaled\HistogramScaled_',Labels_Train{i}), 'png')
+    close;
+end
+
+%% Ratio Histograms
+for i = 1:n
+    edges = linspace(min(Features_Train(:,i)),max(Features_Train(:,i)),10);
+    G = histc(Good(:,i),edges);
+    B = histc(Bad(:,i),edges);
+    figure;
+    hold on;
+    bar(edges,(B*factor)./G,1);
+    plot(linspace(min(Features_Train(:,i)),max(Features_Train(:,i))),1,'r');
+    title(strcat({'Histogram Ratio (Bad/Good) of '},Labels_Train{i}));
+    xlabel(Labels_Train{i});
+    ylabel('Bad/Good');
+    saveas(gcf, strcat('..\Plots\HistogramRatio\HistogramRatio_',Labels_Train{i}), 'png')
     close;
 end
