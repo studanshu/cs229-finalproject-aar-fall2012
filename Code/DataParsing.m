3be123987f5997dab079f04bc33f6e94fa6c2d8f
saharacremona <saharacremona@gmail.com>
2012-11-16 07:36:16 +0000

    Added Year, Month, and Date features

--- Code/DataParsing.m
+++ Code/DataParsing.m
@@ -9,30 +9,29 @@ clear;
 clc;
 close all;
 
+%% Import Numeric Data
+
 % Is this run for the test or training set?
 %   1-Training Set, 0-Test Set
 Set = 1;
 
-% Read in the appropriate data set from the excel sheets
+% Import numeric data into matrix
 if Set == 1
-    [Numeric,Txt,Raw] = xlsread('training.xlsx');
+    [Numeric,~,~] = xlsread('training.xlsx');
 else
-    [Numeric,Txt,Raw] = xlsread('test.xlsx');
+    [Numeric,~,~] = xlsread('test.xlsx');
 end
 
-%%
-clc;
-
-% Setting up variables for easier manipulation
-M = size(Numeric,1);
-N = size(Numeric,2);
-Labels = cell(N,1);
-for i = 1:N
+%% Numeric Data Parsing
+% Set up variables for easier manipulation
+[m,n] = size(Numeric);
+Labels = cell(n,1);
+for i = 1:n
     Labels{i,1} = Txt{1,i};
 end
 ParsedData = Numeric;
 
-% Columns of data that will be knocked out of the data set
+% Columns of data containing non-numeric or irrelevant data that will be knocked out of the data set
 Knockout = [1,3:4,7:12,14,16:18,27:28,31];
 if Set == 0
     Knockout = Knockout - 1;
@@ -46,10 +45,6 @@ if Set == 0
     WhTyID = WhTyID -1;    
     MMR = MMR -1;
 end
-
-% Convert Dates into Day, Month, and Year
-% ParsedData(:,N+1) = zeros(M,1);
-% Knockout = [Knockout, 3];
 
 % Replace empty values in WheelTypeID column with 4's
 ParsedData(isnan(ParsedData(:,WhTyID)),WhTyID) = 4;
@@ -68,16 +63,52 @@ for i = Knockout
     ParsedData(:,i) = [];
 end
 TempLabel = Labels;
-Labels = cell(N-length(Knockout),1);
+Labels = cell(n-length(Knockout),1);
 index = 1;
-for i = 1:N
+for i = 1:n
     if (isempty(TempLabel{i})==0)
         Labels{index} = TempLabel{i};
         index = index + 1;
     end
 end
 
-%%
+%% Import Non-Numeric Data
+% Import all data into cells, extracting spreadsheet dates in MATLAB datenum format
+clear all;
+Set = 1;
+
+if Set == 1
+    [~, ~, ~, dateNums] = xlsread('training.csv','training','','',@convertSpreadsheetDates);
+else
+    [~, ~, ~, dateNums] = xlsread('test.csv','training','','',@convertSpreadsheetDates);
+end
+
+%% Non-Numeric Data Parsing
+
+% Generate year, month, and day features
+% Replace date strings by MATLAB datenums
+% R = ~cellfun(@isequalwithequalnans,dateNums,raw) & cellfun('isclass',raw,'char'); % Find Excel dates
+% raw(R) = dateNums(R);
+
+dateNumVec = dateNums(2:end,3);
+dateNumVec = cell2mat(dateNumVec);
+[year month day] = datevec(dateNumVec);
+
+% % Replace specified string with 1.0
+% R = cellfun(@(x) ischar(x) && strcmp(x,'MAZDA'),raw); % Find non-numeric cells
+% raw(R) = {1.0}; % Replace non-numeric cells
+% 
+% % Replace non-numeric cells with 0.0
+% R = cellfun(@(x) ~isnumeric(x) || isnan(x),raw); % Find non-numeric cells
+% raw(R) = {0.0}; % Replace non-numeric cells
+% 
+% % Create output variables
+% Numeric = cell2mat(raw);
+
+% Clear temporary variables
+% clearvars raw dateNums R;
+
+%% Clean Up and Save Parsed Data
 if Set == 1
     Labels_Train = Labels;
     Features_Train = ParsedData;
