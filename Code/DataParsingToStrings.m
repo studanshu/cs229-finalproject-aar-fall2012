53f8bdbe7281dbb620ee359ee2f30f1cf99bc713
saharacremona <saharacremona@gmail.com>
2012-12-11 03:32:12 +0000

    Added Weka stuff

--- Code/DataParsingToStrings.m
+++ Code/DataParsingToStrings.m
@@ -1,4 +1,41 @@
+%% Setup
 close all;
 clear all;
 
-load('CarAuction_Parsed_Train.mat');
+% Is this run for the test or training set?
+%   1-Training Set, 0-Test Set
+Set = 0;
+
+if Set == 1
+    load('CarAuction_Parsed_Train.mat');
+    data = Labels_Train';
+    data = [data; num2cell(Features_Train)];
+    start = 18;
+else
+    load('CarAuction_Parsed_Test.mat');
+    data = num2cell(Features_Test);
+    start = 17;
+end
+    
+%% Replace isBadBuy with true/false
+for i = 2:size(data,1)
+    if data{i,1} == 0
+        data{i,1} = 'false';
+    else
+        data{i,1} = 'true';
+    end
+end
+
+%% Replace rest of categorical dataset
+for i = 2:size(data,1)
+    for j = start:size(data,2)
+        data{i,j} = strcat('a', num2str(data{i,j}));
+    end
+end
+
+%% Write to CSV
+if Set == 1
+    cell2csv('CarAuction_Parsed_Train_Str.csv',data);
+else
+    cell2csv('CarAuction_Parsed_Test_Str.csv',data);
+end
